ea8f03fa2c781c6e28504d155e8065faf1a63209
chocozz <kthanyarat.b@gmail.com>
2015-04-07 23:00:13 +0700

    change image of blackground

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -8,23 +8,35 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
+// add import
+import java.awt.Toolkit;
+import java.awt.Image;
+
 public class GamePanel extends JPanel {
 	
-	private BufferedImage bi;	
+	private BufferedImage bi;
+	private Image imgBg;	
 	Graphics2D big;
 	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
 
 	public GamePanel() {
 		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
 		big = (Graphics2D) bi.getGraphics();
-		big.setBackground(Color.BLACK);
+		//big.setBackground(Color.BLACK);
+
+		imgBg = Toolkit.getDefaultToolkit().getImage("bg.jpg");
+		big.drawImage(imgBg, 0, 0, 400, 600,null);
 	}
 
 	public void updateGameUI(GameReporter reporter){
 		big.clearRect(0, 0, 400, 600);
 		
-		big.setColor(Color.WHITE);		
-		big.drawString(String.format("%08d", reporter.getScore()), 300, 20);
+		big.setColor(Color.YELLOW);		
+		big.drawString(String.format("%08d", reporter.getScore()), 250, 20);
+
+		//EXTEND  DrawImage 
+        big.drawImage(imgBg, 0, 0, 400, 600,null);
+
 		for(Sprite s : sprites){
 			s.draw(big);
 		}
